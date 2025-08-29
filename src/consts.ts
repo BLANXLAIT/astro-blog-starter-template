c3927eb6260e06b76fd7ffdf1f3559030ab315bc
Ryan William Niemes <ryan.niemes@gmail.com>
2025-08-28 23:47:10 -0400

    second commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "TechTalent Recruiting";
+export const SITE_DESCRIPTION = "Connecting top IT professionals with leading technology companies. Expert recruitment for software engineers, developers, DevOps, and tech leadership roles.";
+
+// Company Info
+export const COMPANY_NAME = "TechTalent Recruiting";
+export const COMPANY_TAGLINE = "Your Bridge to Tech Excellence";
+export const CONTACT_EMAIL = "hello@techtalent.com";
+export const CONTACT_PHONE = "(555) 123-4567";
